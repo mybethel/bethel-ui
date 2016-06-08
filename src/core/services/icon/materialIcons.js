0e69df0cc4642f585a4589d2132fbb29610527bc
Albert Martin <amartin@treehouse.co>
2016-06-07 20:41:09 -0500

    chore(icon): add location icon

--- src/core/services/icon/materialIcons.js
+++ src/core/services/icon/materialIcons.js
@@ -1,6 +1,7 @@
 module.exports = {
   av: ['mic', 'play_circle_filled', 'videocam'],
   action: ['dashboard', 'exit_to_app', 'settings', 'thumb_up', 'verified_user'],
+  communication: ['location_on'],
   editor: ['attach_money'],
   hardware: ['phone_iphone'],
   navigation: ['arrow_drop_down', 'arrow_drop_up', 'close', 'menu'],

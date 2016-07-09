b01c04d1c0f847ca06a89487303e5a159bdedd17
Albert Martin <amartin@treehouse.co>
2016-07-09 18:49:23 -0500

    chore(topBar): make user menu functional

--- src/components/topBar/topBar.component.js
+++ src/components/topBar/topBar.component.js
@@ -23,8 +23,8 @@ angular.module('bethel.ui.topBar', []).component('topBar', {
 // --
 function TopNavComponent($rootScope, $state) {
 
-  var $ctrl = this;
-
+  // The title of the page is determined by the `$state` data. If a title is not
+  // set or the state cannot be determined, the title remains empty.
   this.$postLink = () => {
     if (!$state.current || !$state.current.data) return;
     this.title = $state.current.data.pageTitle || '';
@@ -34,13 +34,22 @@ function TopNavComponent($rootScope, $state) {
   // update the title of the navbar. The title shown is provided by the state
   // configuration for that route.
   // https://github.com/angular-ui/ui-router/wiki#state-change-events
-  $rootScope.$on('$stateChangeSuccess', function(event, toState) {
-    $ctrl.title = toState.data && toState.data.pageTitle || '';
+  $rootScope.$on('$stateChangeSuccess', (event, toState) => {
+    this.title = toState.data && toState.data.pageTitle || '';
   });
 
   // Emit an event to toggle between the expanded and collapsed nav states.
-  this.toggleNav = function() {
-    $rootScope.$emit('bethel:nav:toggle', '');
+  this.toggleNav = () => {
+    $rootScope.$emit('bethel:nav:toggle');
+  };
+
+  // Both of these functions allow the user menu to trigger state transition or
+  // load the logout route on the server. @TODO: This solution is not
+  // sufficiently isolated to be a component; investigate doing this through a
+  // multi-slot transclusion.
+  this.userMenu = $state.go;
+  this.logout = () => {
+    window.location.href = '/session/destroy';
   };
 
 }

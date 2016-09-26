093713178bf7bbb4530a4eb52d458086812c0e36
Albert Martin <albert@imor.tl>
2016-09-25 22:43:00 -0500

    feat(expansionPanel): prefer triggering the cancel button click handler

--- src/components/expansion-panel/expansion-panel.component.js
+++ src/components/expansion-panel/expansion-panel.component.js
@@ -10,9 +10,20 @@ angular.module('bethel.ui.expansionPanel', []).component('expansionPanel', {
     this.ngModel.$toggle = () => {
       this.ngModel.$expanded = !this.ngModel.$expanded;
       $element[this.ngModel.$expanded ? 'addClass' : 'removeClass']('expanded');
-      $scope.$apply();
+      $scope.$applyAsync();
     };
 
-    $element.find('header').on('click', this.ngModel.$toggle);
+    // On click of the header element we toggle the expanded state of the panel.
+    // If the panel is expanded and a button has the attribute `cancel`, the
+    // click handler of this element will be triggered instead. It is assumed
+    // that the click handler for this element calls the `$toggle()` function.
+    $element.find('header').on('click', () => {
+      var cancelButton = $element[0].querySelectorAll('button[cancel]');
+      if (cancelButton[0] && $element.hasClass('expanded')) {
+        angular.element(cancelButton[0]).triggerHandler('click');
+        return;
+      }
+      this.ngModel.$toggle();
+    });
   }]
 });
